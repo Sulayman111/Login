6f29eb0085200d69ce8363b4b83f2dae40254d10
Sulayman <sulaymansatbayev222@gmail.com>
2023-02-11 12:45:18 +0300

    Update Login

--- tkinter_github.py
+++ tkinter_github.py
@@ -10,30 +10,52 @@ def name():
     webbrowser.open_new_tab(url)
 
 window = tkinter.Tk()
-window.title('Email and password')
-window.geometry('480x400')
+window.title('Login')
+window.geometry('411x508')
 window.resizable(False, False)
-window['bg'] = '#696969'
+window['bg'] = '#1F1F1F'
+
+frame = tkinter.Frame(window, width=360, height=460, bg="White")
+frame.place(x=25, y=23)
+frame['bg'] = '#3B3B3B'
+
+show_code = '😀'
+hide_code = '😄'
+
+def show_hide_code():
+    if ent2['show'] == '●':
+        ent2.configure(show='')
+        but2.configure(text=show_code, bg='green')
+    else:
+        ent2.configure(show='●')
+        but2.configure(text=hide_code)
 
 img = tkinter.PhotoImage(file = "./Tkinter/image/github.png")
 imgbx = tkinter.Label(image = img)
-imgbx.place(x=195, y=15)
-imgbx['bg'] = '#696969'
+imgbx.place(x=165, y=45)
+imgbx['bg'] = '#3B3B3B'
 
-label1 = tkinter.Label(text = "Email: ")
-label1.place(x = 100, y = 170)
-label1['bg'] = '#696969'
-label1['font'] = 'Roboto 10 bold'
-ent1 = ttk.Entry(window)
-ent1.place(x=170, y=170)
+label1 = tkinter.Label(frame,text = "Username / email: ")
+label1.place(x = 20, y = 160)
+label1.config(background='#3B3B3B', foreground='white')
+label1['font'] = 'NORMAL 12'
+ent1 = ttk.Entry(frame, font='Roboto 12')
+ent1.place(x=40, y=185, width=280)
+ent1.focus()
 
-label2 = tkinter.Label(text = "Password: ")
-label2.place(x = 100, y = 240)
-label2['bg'] = '#696969'
-label2['font'] = 'Roboto 10 bold'
-ent2 = ttk.Entry(window)
-ent2.place(x=170, y=240)
+label2 = tkinter.Label(frame, text = "Password: ")
+label2.place(x = 20, y = 245)
+label2.config(background='#3B3B3B', foreground='white')
+label2['font'] = 'NORMAL 12'
+ent2 = ttk.Entry(frame, show="●", font='Roboto 12')
+ent2.place(x=40, y=270, width=280)
 
-but1 = ttk.Button(text='Login', command=name)
-but1.place(x=190, y=300)
+but1 = tkinter.Button(frame, text='Login', command=name)
+but1.place(x=120, y=330, width=120)
+but1.config(background='green', foreground='white')
+
+but2 = tkinter.Button(frame, text=hide_code, command=show_hide_code)
+but2.place(x=325, y=270, width=20, height=23)
+but2['bg'] = 'red'
+
 window.mainloop()
